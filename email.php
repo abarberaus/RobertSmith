d08d6507a8a481fddc677657c306dffc950b03f7
abarberaus <abarberaus@gmail.com>
2015-07-18 12:39:10 -0500

    pushing lenny files over

---
+++ email.php
@@ -0,0 +1,17 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "leonard@lmoraleslaw.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name']; // this is the sender's Name
+    $phone = $_POST['phone']; // this is the sender's Phone Number
+    $the_message = $_POST['message']; // this is the sender's Message
+    $subject = "Form submission";
+    $message = $name . " " . " from " . $from . "." . " They have this phone number: " . $phone . ", and their message is this: " . $the_message;
+
+    mail($to,$subject,$message);
+    header("Location: thank_you.php");
+
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
---
+++ volunteer.php
@@ -0,0 +1,134 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Leonard Morales for District Attorney</title>
+        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+        <!-- Custom Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+        <!-- Plugin CSS -->
+        <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="css/creative.css" type="text/css">
+        <!-- My CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body id="page-top">
+        <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="index.html"><span class="white">Leonard Morales<span class="disappear_small"> for District Attorney</span></span></a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-left">
+                        <li>
+                            <a class="page-scroll" href="lenny.html">About Lenny</a>
+                        </li>
+                        <!-- Priorities page to be updated with Lenny's content
+                        <li>
+                            <a class="page-scroll" href="priorities.html">Priorities</a>
+                        </li>
+                         -->
+                        <li>
+                            <a class="page-scroll" href="volunteer.php">Volunteer</a>
+                        </li>
+                    </ul>
+                     <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                            <input type="hidden" name="cmd" value="_s-xclick">
+                            <input type="hidden" name="hosted_button_id" value="B27YPKLAR27PC">
+                            <input type="submit" value="Donate Now" name="submit" title="PayPal - The safer, easier way to pay online!" class="paypal_nav">
+                            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                        </form>
+                        </li>
+                        <li><a href="https://www.facebook.com/lennyforda" style="padding-right: 0px; padding-top: 12px;"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+                        <li><a href="https://twitter.com/Lenny4DA" style="padding-top: 12px;"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+        <!-- Section 1 -->
+        <header>
+            <div class="container v1">
+                <div class="row">
+                    <div class="box">
+                        <div class="col-lg-12">
+                            <h2>Thank You for Your Interest in Volunteering!</h2>
+                            <hr>
+                        </div>
+                        <div class="col-md-12">
+                            <p>Please fill out the information below, and we'll contact you as soon as possible.</p>
+                            <form action="email.php" method="post" role="form">
+                                <div class="row">
+                                    <div class="form-group col-lg-4">
+                                        <label>Name</label>
+                                        <input type="text" class="form-control" name="name" required>
+                                    </div>
+                                    <div class="form-group col-lg-4">
+                                        <label>Email Address</label>
+                                        <input type="email" class="form-control" name="email" required>
+                                    </div>
+                                    <div class="form-group col-lg-4">
+                                        <label>Phone Number</label>
+                                        <input type="tel" class="form-control" name="phone">
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    <div class="form-group col-lg-12">
+                                        <label>Message</label>
+                                        <textarea class="form-control" rows="6" name="message"></textarea>
+                                    </div>
+                                    <div class="form-group col-lg-12">
+                                        <input type="hidden" name="save" value="contact">
+                                        <button type="submit" name="submit" class="btn btn-default v_button">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <footer class="footer">
+            <div class="container">
+                <p>Copyright &copy; 2015. All Rights Reserved Leonard Morales</p>
+            </div>
+        </footer>
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Plugin JavaScript -->
+        <script src="js/jquery.easing.min.js"></script>
+        <script src="js/jquery.fittext.js"></script>
+        <script src="js/wow.min.js"></script>
+        <!-- Custom Theme JavaScript -->
+        <script src="js/creative.js"></script>
+    </body>
+</html>
